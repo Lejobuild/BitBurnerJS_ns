daab72f4612db59ff5f44c00a99124c49484ff33
Lejobuild <78205960+Lejobuild@users.noreply.github.com>
2023-03-11 18:19:49 +0100

    Updated SigmaHack.js
    I created it as a new file so you can better see what updates i created

---
+++ SigmaHack2_0.js
@@ -0,0 +1,22 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    ns.print("Starting script here");
+    while (true) {
+        await ns.grow("sigma-cosmetics"); //Use Netscript grow function
+        ns.print(ns.args);           //The script arguments must be prefaced with ns as well
+        await ns.weaken("sigma-cosmetics"); //Use Netscript weaken function
+        ns.print(ns.args);           //The script arguments must be prefaced with ns as well
+        await ns.grow("sigma-cosmetics"); //Use Netscript grow function
+        ns.print(ns.args);           //The script arguments must be prefaced with ns as well
+        await ns.weaken("sigma-cosmetics"); //Use Netscript weaken function
+        ns.print(ns.args);           //The script arguments must be prefaced with ns as well
+        await ns.grow("sigma-cosmetics"); //Use Netscript grow function
+        ns.print(ns.args);           //The script arguments must be prefaced with ns as well
+        await ns.weaken("sigma-cosmetics"); //Use Netscript weaken function
+        ns.print(ns.args);           //The script arguments must be prefaced with ns as well
+        await ns.hack("sigma-cosmetics"); //Use Netscript weaken function
+        ns.print(ns.args);           //The script arguments must be prefaced with ns as well
+        await ns.weaken("sigma-cosmetics"); //Use Netscript weaken function
+        ns.print(ns.args);           //The script arguments must be prefaced with ns as well
+    }
+}
